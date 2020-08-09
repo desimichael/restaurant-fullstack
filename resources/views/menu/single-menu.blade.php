ff6748056a611244dc7e0032f60a9b40c41a2434
desimichael <desiwebdev11@gmail.com>
2020-08-09 18:04:15 -0400

    Created single page menu with html & css!

---
+++ resources/views/menu/single-menu.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+<div id="singleMenuPage">
+  <div class="row">
+    <div class="col-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8">
+      <div class="contentBox">
+        <div class="col-md-12">
+          <div class="title">
+            Burgers
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="item">
+              <div class="subtitle">   
+                <h5>Bacon Burger</h5>
+                <span class="price">$10</span>
+              </div>
+              <div class="description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, natus.
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="item">
+              <div class="subtitle">   
+                <h5>Bacon Burger</h5>
+                <span class="price">$10</span>
+              </div>
+              <div class="description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, natus.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="item">
+              <div class="subtitle">   
+                <h5>Bacon Burger</h5>
+                <span class="price">$10</span>
+              </div>
+              <div class="description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, natus.
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="item">
+              <div class="subtitle">   
+                <h5>Bacon Burger</h5>
+                <span class="price">$10</span>
+              </div>
+              <div class="description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, natus.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="item">
+              <div class="subtitle">   
+                <h5>Bacon Burger</h5>
+                <span class="price">$10</span>
+              </div>
+              <div class="description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, natus.
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="item">
+              <div class="subtitle">   
+                <h5>Bacon Burger</h5>
+                <span class="price">$10</span>
+              </div>
+              <div class="description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, natus.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
